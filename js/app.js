23ce9390db821a706b2ff72a83d16849b349b39a
Kosal <sean.kosal@outlook.com>
2019-10-28 14:49:32 +0700

    aleady

---
+++ js/app.js
@@ -0,0 +1,7 @@
+    $(function () {
+        var typed = new Typed('.main', {
+            strings: ["COMING SOON", "មកដល់ឆាប់ៗនេះ", "COMING SOON"],
+            loop: true,
+            typeSpeed: 60
+        });
+    });
